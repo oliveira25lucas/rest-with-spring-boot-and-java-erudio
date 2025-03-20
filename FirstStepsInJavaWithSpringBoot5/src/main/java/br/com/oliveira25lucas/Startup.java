0c4926273d80bff981acc4a3b3f97b0eb8b38097
Lucas Oliveira <lucas1122oliveira@gmail.com>
2025-03-20 17:54:06 -0300

    Removendo arquivos antigos e adicionando novas versões

---
+++ FirstStepsInJavaWithSpringBoot5/src/main/java/br/com/oliveira25lucas/Startup.java
@@ -0,0 +1,14 @@
+package br.com.oliveira25lucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
