a1c607141c0925d7038245b58479e6a475a03a0b
Lucas Oliveira <lucas1122oliveira@gmail.com>
2025-03-31 16:25:41 -0300

    Secao 17

--- FirstStepsInJavaWithSpringBoot4/src/main/java/br/com/oliveira25lucas/exception/ExceptionResponse.java
+++
@@ -1,5 +0,0 @@
-package br.com.oliveira25lucas.exception;
-
-import java.util.Date;
-
-public record ExceptionResponse(Date timestamp, String message, String details) {}
--- FirstStepsInJavaWithSpringBoot4/src/main/java/br/com/oliveira25lucas/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-
-package br.com.oliveira25lucas.repository;
-
-import br.com.oliveira25lucas.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {}
